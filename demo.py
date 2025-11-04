ee5eb1b6651f69841657b28cc24db2d87dda7091
Siphesihle SEhume <sisehjhb025@student.wethinkcode.co.za>
2025-11-04 15:17:16 +0200

    First commit this year

---
+++ demo.py
@@ -0,0 +1,15 @@
+def fizzbuzz(n: int):
+    """
+    Returns a list for the FizzBuzz game up to n.
+    - Multiples of 3 are "Fizz"
+    - Multiples of 5 are "Buzz"
+    - Multiples of both 3 and 5 are "FizzBuzz"
+    """
+    # TODO: Implement the logic
+   
+
+    
+    #return fizz_game
+
+
+print(fizzbuzz(10))
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def factorial(n):
     """Calculates the factorial of a non-negative integer."""
     # TODO: Implement the logic
-    pass 
+    nums = [1, 1]
+
+    while len(nums) < n:
+        nums.append(nums[-1] + nums[-2])
+    return nums
 
 def fibonacci(n):
     """Returns the nth number in the Fibonacci sequence."""
     # TODO: Implement the logic
-    return None
+    if n <= 0:
+        return 1
+    else:
+        return n * fibonacci(n-1)
 
 def fizzbuzz(n):
     """
@@ -16,4 +23,12 @@ def fizzbuzz(n):
     - Multiples of both 3 and 5 are "FizzBuzz"
     """
     # TODO: Implement the logic
-    return []
+    fizz_game = []
+    multiple_of_3 = [fizz_game.append("Fizz") for count in range(3, n, 3)]
+    multiple_of_5 = [fizz_game.append("Buzz") for count in range(5, n, 5)]
+    
+    if multiple_of_3 and multiple_of_5:
+        fizz_game.append("FizzBuzz")
+    
+    
+    return fizz_game
